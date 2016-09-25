fa216b0de9dc5eb660e62c18bfb58a0cb68d37fe
cfloryiv <cfloryiv@rcn.com>
2016-09-24 21:29:08 -0400

    prep repo for heroku

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 var express=require('express');
 
 var app=express();
+const property=process.env.PORT || 3000;
 
+app.use(function(req, res, next) {
+  if (req.headers[]'x-forwarded-proto']==='http') {
+    next();
+  } else {
+    res.redirect('http://'+req.hostname+req.url);
+  }
+});
 app.use(express.static('public'));
 
-app.listen(3000, function() {
-  console.log('express is running on port 3000');
+app.listen(PORT, function() {
+  console.log('express is running on port '+PORT');
 });
