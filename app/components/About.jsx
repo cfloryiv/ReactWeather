b86451e5aa212d2fe8ec474ba9ab5cf9bb8a7257
cfloryiv <cfloryiv@rcn.com>
2016-09-27 11:28:44 -0400

    filled out About page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -3,7 +3,8 @@ var About=(props) => {
   return (
     <div>
       <h2>About</h2>
-      <p>Welcome to the About page</p>
+      <p>This application does a lookup by city and returns the current temperature.</p>
+      <a href="http://github.com/cfloryiv/ReactWeather">This is the Code Page</a>
     </div>
 
   )
